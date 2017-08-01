a82318e1fd201dd0248f55c32f6c33e6f83eb92e
System <none>
2017-08-01 15:48:43 +0000

    Import of upperlinecode/p5-starter-template

---
+++ sketch.js
@@ -0,0 +1,12 @@
+
+// The setup function runs once
+function setup() {
+  
+  //createCanvas(800, 600);
+  //background(0);
+}
+
+//The draw function runs many times per second. The default rate is 60 frames per second!
+function draw() {
+ 
+}
